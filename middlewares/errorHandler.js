a9b2f357ad2181d7a15ed9f8d17f88453441cdb1
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2021-08-19 17:30:34 +0300

    fix-project-error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || 400;
-  const message = statusCode === 400 ? 'Произошла ошибка' : err.message;
+  const message = statusCode === 400 ? 'Произошла ошибка валидации' : err.message;
   res.status(statusCode).send({ message });
   next();
 };
